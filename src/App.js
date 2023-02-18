dcef2b6a2324ba181cbd11d6108b8995ba9b8939
Fajar Satria <fajarsat@hotmail.com>
2023-02-18 21:48:07 +0700

    add navbar
    add navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
-import counters from './components/counters'
-import Movies from './components/movies';
+import React, { Component } from 'react';
+// import counters from './components/counters'
+// import Movies from './components/movies';
 import Counters from './components/counters';
+import Navbar from './components/navbar';
 
 class App extends Component{
-  render(){
+  render()  {
     return (
-      <main className="container">
-        {/* <Movies/> */}
-        <Counters/>
-      </main>
+        <React.Fragment>
+          <Navbar />
+          <main className="container">
+            <Counters/>
+          </main>
+        </React.Fragment>
     );
   }
 }
