552d75b1c71c568f3e21204bcf3f85971b2b952c
Fajar Satria <fajarsat@hotmail.com>
2023-02-18 20:38:57 +0700

    learn props vs state
    learn props vs state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import counters from './components/counters'
 import Movies from './components/movies';
+import Counters from './components/counters';
 
 class App extends Component{
   render(){
     return (
       <main className="container">
-        <Movies/>
+        {/* <Movies/> */}
+        <Counters/>
       </main>
     );
   }
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Couter extends Component {
     state = {
-        count: 0,
+        value: this.props.value,
         tags: ['tag1', 'tag2', 'tag3']
     };
 
@@ -11,11 +11,6 @@ class Couter extends Component {
         return <ul> {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
     }
 
-    handleClick = (product) => {
-        console.log(product);
-        this.setState({count: this.state.count+1});
-        console.log('Clicked!', this.state.count);
-    }
 
     render() { 
         return(
@@ -27,18 +22,19 @@ class Couter extends Component {
                     className="btn btn-secondary btn-sm">
                 Test
                 </button>
+                <button onClick={this.props.onDelete} className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
         );
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const { count } = this.state;
+        const { value: count } = this.state;
         return count === 0 ? 'Zero' : count;
     }
 }
