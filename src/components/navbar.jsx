a7ac16f6c7fee9923fdb73b3dd5cf2bc2df89715
Fajar Satria <fajarsat@hotmail.com>
2023-02-18 21:56:58 +0700

    add counters to navbar
    add counters to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,6 +4,7 @@ class Navbar extends Component {
         return (
             <nav className="navbar navbar-light bg-light">
                 <a className="navbar-brand" href="#">Navbar</a>
+                <h1>{this.props.totalCounters}</h1>
             </nav>
         );
     }
