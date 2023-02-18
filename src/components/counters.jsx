4c6b0ef76f5d29835303789f5a4101b03f978ef8
Fajar Satria <fajarsat@hotmail.com>
2023-02-18 22:07:06 +0700

    destructuring
    destructuring

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,16 +2,17 @@ import React, { Component } from 'react'
 import Counter from './counter'
 class Counters extends Component {
     render() { 
+        const {onReset, onIncrement, counters, onDelete} = this.props;
         return (<div>
-            <button onClick={this.props.onReset} className="btn btn-primary btn-sm m-2">
+            <button onClick={onReset} className="btn btn-primary btn-sm m-2">
                 Reset
             </button>
-            {this.props.counters.map(counter => 
+            {counters.map(counter => 
             <Counter 
                 key={counter.id} 
                 counter={counter}
-                onDelete={this.props.onDelete}
-                onIncrement={this.props.onIncrement}
+                onDelete={onDelete}
+                onIncrement={onIncrement}
                 />)}
         </div>);
     }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,13 @@
-import React, { Component } from 'react'
-class Navbar extends Component {
-    render() { 
-        return (
-            <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="#">Navbar {" "}
-                    <span className="badge badge-spill badge-secondary">{this.props.totalCounters}</span>
-                </a>
-            </nav>
-        );
-    }
+const Navbar = ({totalCounters}) => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">Navbar {" "}
+                <span className="badge badge-spill badge-secondary">
+                    {totalCounters}
+                </span>
+            </a>
+        </nav>
+    );
 }
  
 export default Navbar;
