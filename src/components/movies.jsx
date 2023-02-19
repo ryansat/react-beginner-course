57682c86079c4cc7b093dab73d0f7d1a2b723b65
Fajar Satria <fajarsat@hotmail.com>
2023-02-19 20:06:50 +0700

    Update movies.jsx
    refactoring code

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -47,11 +47,8 @@ class Movies extends Component {
         this.setState({ sortColumn });
     }
 
-    render() {
-        const {length:count} = this.state.movies;
+    getPageData = () =>{
         const {pageSize, currentPage, movies:allMovies, selectedGenre, sortColumn} = this.state;
-        if (count === 0) return <p>there is no movies</p>
-
         const filtered =  selectedGenre && selectedGenre._id
         ? allMovies.filter(m => m.genre._id === selectedGenre._id) 
         : allMovies;
@@ -60,6 +57,15 @@ class Movies extends Component {
 
         const movies = paginate(sorted,currentPage,pageSize);
 
+        return {totalCount: filtered.length, data:movies};
+    }
+
+    render() {
+        const {length:count} = this.state.movies;
+        const {pageSize, currentPage, sortColumn} = this.state;
+        if (count === 0) return <p>there is no movies</p>
+
+        const {totalCount, data: movies} = this.getPageData();
         return (
             <div className='row'>
                 <div className="col-3">
@@ -70,7 +76,7 @@ class Movies extends Component {
                     />
                 </div>
                 <div className="col">
-                <p>showing {filtered.length} movies in database</p>
+                <p>showing {totalCount} movies in database</p>
                     <MoviesTable 
                         movies={movies} 
                         onLike={this.handleLike} 
@@ -79,7 +85,7 @@ class Movies extends Component {
                         sortColumn={sortColumn}
                     />
                     <Pagination 
-                        itemsCount={filtered.length} 
+                        itemsCount={totalCount} 
                         pageSize={pageSize} 
                         onPageChange={this.handlePageChange}
                         currentPage={currentPage}
