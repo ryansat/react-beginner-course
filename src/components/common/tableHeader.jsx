8402553f1e1f668d0c21213d2f1c60fa4489cd51
Fajar Satria <fajarsat@hotmail.com>
2023-02-19 20:00:37 +0700

    extract table to a component
    extract table to a component

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class TableHeader extends Component {
     raiseSort = path =>{
@@ -13,12 +14,24 @@ class TableHeader extends Component {
         this.props.onSort(sortColumn);
     }
 
+    renderSortIcon = column => {
+        const {sortColumn} = this.props;
+        if (column.path !== sortColumn.path){
+            return null;
+        }
+        if (sortColumn.order === 'asc'){
+            return <FontAwesomeIcon icon="fas fa-sort-up"/>
+        }
+        return <FontAwesomeIcon icon="fas fa-sort-down"/>
+    }
+
     render() { 
         return (
             <thead>
                 <tr>
                     {this.props.columns.map(column => 
-                        <th key={column.path || column.key} onClick={() => this.raiseSort(column.path)}>{column.label}</th>)}
+                        <th className='clickable' key={column.path || column.key} onClick={() => 
+                        this.raiseSort(column.path)}>{column.label} {this.renderSortIcon(column)}</th>)}
                 </tr>
             </thead>
         );
